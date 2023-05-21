6c288b4e19d4572b39d038a539b1793382bd63ca
Raju kumar <rajuvis778@gmail.com>
2023-05-21 07:49:14 +0530

    Added navbar , createContext to the frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import About from './components/About';
+//importing the react router dom
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+import NoteState from './context/notes/NotesState';
+
+function App() {
+  return (
+    <>
+      <NoteState>
+        <Router>
+          <Navbar />
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route exact path="/about">
+              <About />
+            </Route>
+          </Switch>
+        </Router>
+      </NoteState>
+    </>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/About.jsx
@@ -0,0 +1,13 @@
+import React, { useContext, useEffect} from 'react'
+import NoteContext from '../context/notes/NoteContext'
+
+const About = () => {
+
+  return (
+    <div>
+      this is about {a.state.name} and he reads in class {a.state.class}
+    </div>
+  )
+}
+
+export default About
